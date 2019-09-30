aa4a2bd4061e3a211a4b2cbcae21b9f047376c2d
Xanonymous <trusaidlin@gmail.com>
2019-10-01 01:13:53 +0800

    NPC=?

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,9 @@
 #include<cstdio>
 int main(int argc,char* argv[]){
-    printf("Hello World!");
+    printf("Hello World!\n");
     //I'm Xanonymous XD~~
+    for (;;){
+        printf("NPC\n");
+    } 
     return 0;
 }
